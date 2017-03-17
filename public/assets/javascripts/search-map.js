73fdfc0847175b9204ce1c574d359225a4c7bf8b
Tommy Thompson <tomthompson828@gmail.com>
2017-03-16 20:58:12 -0400

    raised mapping wait timer

--- public/assets/javascripts/search-map.js
+++ public/assets/javascripts/search-map.js
@@ -89,7 +89,7 @@ $(document).ready(function(){
     .done(function(response){
 
       // Re run mapping function (global variable from the map-search-letter.js script)
-      setTimeout(function(){reInitMap(response)}, 750); // hacky fix to allow enough time for initMap() to fire, was 2100
+      setTimeout(function(){reInitMap(response)}, 1100); // hacky fix to allow enough time for initMap() to fire, was 2100
 
     });
 
